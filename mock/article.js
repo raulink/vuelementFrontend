fef4c28ba2064aab71ca2278d20c142a3032a4a3
ramamani <ramamani@miteleferico.bo>
2021-06-16 14:59:00 -0400

    complex table con campos reducidos

--- mock/article.js
+++ mock/article.js
@@ -1,7 +1,7 @@
 const Mock = require('mockjs')
 
 const List = []
-const count = 100
+const count = 21
 
 const baseContent = '<p>I am testing data, I am testing data.</p><p><img src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943"></p>'
 const image_uri = 'https://wpimg.wallstcn.com/e4558086-631c-425c-9430-56ffb46e70b3'
@@ -18,7 +18,7 @@ for (let i = 0; i < count; i++) {
     forecast: '@float(0, 100, 2, 2)',
     importance: '@integer(1, 3)',
     'type|1': ['CN', 'US', 'JP', 'EU'],
-    'status|1': ['published', 'draft'],
+    'status|1': ['publicado', 'borrador'],
     display_time: '@datetime',
     comment_disabled: true,
     pageviews: '@integer(300, 5000)',
--- src/views/table/.temp/tablaConEstrellas.vue
+++ src/views/table/.temp/tablaConEstrellas.vue
@@ -24,7 +24,7 @@
         Revisor
       </el-checkbox>
     </div>
-<!-- Tabla de Datos -->
+
     <el-table
       :key="tableKey"
       v-loading="listLoading"
@@ -35,26 +35,43 @@
       style="width: 100%;"
       @sort-change="sortChange"
     >
-
       <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80" :class-name="getSortClass('id')">
         <template slot-scope="{row}">
           <span>{{ row.id }}</span>
         </template>
       </el-table-column>
-
-      <el-table-column label="Titulo" min-width="150px">
+<!--       <el-table-column label="Fecha" width="150px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+        </template>
+      </el-table-column> -->
+      <el-table-column label="Title" min-width="150px">
         <template slot-scope="{row}">
           <span class="link-type" @click="handleUpdate(row)">{{ row.title }}</span>
           <el-tag>{{ row.type | typeFilter }}</el-tag>
         </template>
       </el-table-column>
-      
+      <!-- <el-table-column label="Author" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.author }}</span>
+        </template>
+      </el-table-column> -->
       <el-table-column v-if="showReviewer" label="Revisor" width="110px" align="center">
         <template slot-scope="{row}">
           <span style="color:red;">{{ row.reviewer }}</span>
         </template>
       </el-table-column>
-      
+      <el-table-column label="Imp" width="80px">
+        <template slot-scope="{row}">
+          <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="meta-item__icon" />
+        </template>
+      </el-table-column>
+      <!-- <el-table-column label="Readings" align="center" width="95">
+        <template slot-scope="{row}">
+          <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
+          <span v-else>0</span>
+        </template>
+      </el-table-column> -->
       <el-table-column label="Estado" class-name="status-col" width="100">
         <template slot-scope="{row}">
           <el-tag :type="row.status | statusFilter">
@@ -77,33 +94,49 @@
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
 
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="150px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="Tipo de Datos" prop="type">
-          <el-select v-model="temp.type" class="filter-item" placeholder="Tipo de Dato">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="Type" prop="type">
+          <el-select v-model="temp.type" class="filter-item" placeholder="Please select">
             <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
         </el-form-item>
         <el-form-item label="Fecha" prop="timestamp">
-          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Fecha" />
+          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />
         </el-form-item>
-        <el-form-item label="Descripcion" prop="title">
+        <el-form-item label="Titulo" prop="title">
           <el-input v-model="temp.title" />
         </el-form-item>
-        <el-form-item label="Estado">
-          <el-select v-model="temp.status" class="filter-item" placeholder="Estado">
+        <el-form-item label="Status">
+          <el-select v-model="temp.status" class="filter-item" placeholder="Please select">
             <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item" />
           </el-select>
         </el-form-item>
-        <el-form-item label="Comentarios">
-          <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Comentarios" />
+        <el-form-item label="Imp">
+          <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />
+        </el-form-item>
+        <el-form-item label="Remark">
+          <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">Cancelar</el-button>
-        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">OK</el-button>
+        <el-button @click="dialogFormVisible = false">
+          Cancelar
+        </el-button>
+        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
+          Confirmar
+        </el-button>
       </div>
     </el-dialog>
-    
+
+    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
+      <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="key" label="Channel" />
+        <el-table-column prop="pv" label="Pv" />
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -190,9 +223,9 @@ export default {
       pvData: [],
       // Validacion de imput
       rules: {
-        type: [{ required: true, message: 'Tipo de datos Requerido', trigger: 'change' }],
-        timestamp: [{ type: 'date', required: true, message: 'Fecha es requerido', trigger: 'change' }],
-        title: [{ required: true, message: 'Descripcion es necesaria', trigger: 'blur' }]
+        type: [{ required: true, message: 'type is required', trigger: 'change' }],
+        timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+        title: [{ required: true, message: 'title is required', trigger: 'blur' }]
       },
       downloadLoading: false
     }
